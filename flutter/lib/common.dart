76ce3c3f0f4dcb4beafd5a19dd2776d7505c1bff
John Martirosian <john@antme.com>
2025-12-22 13:11:41 -0800

    Changed URL to ANT

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3597,7 +3597,7 @@ Widget loadPowered(BuildContext context) {
     cursor: SystemMouseCursors.click,
     child: GestureDetector(
       onTap: () {
-        launchUrl(Uri.parse('https://rustdesk.com'));
+        launchUrl(Uri.parse('https://www.antme.com'));
       },
       child: Opacity(
           opacity: 0.5,
