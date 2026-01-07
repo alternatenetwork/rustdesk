e91fd81b2ef402410cc5c5964db932410d49a151
John Martirosian <john@antme.com>
2026-01-07 11:01:05 -0800

    testing

--- res/customize_macos_app.sh
+++ res/customize_macos_app.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 # Script to customize macOS app name and metadata
-# Usage: ./customize_macos_app.sh <app_name> <manufacturer> <bundle_id_suffix>
+# Usage: ./customize_macos_app.sh <app_name> <manufacturer>
 
 APP_NAME="${1:-ANTConnect}"
 MANUFACTURER="${2:-Alternate Network Technologies}"
-BUNDLE_ID_SUFFIX="${3:-antconnect}"
-BUNDLE_ID_PREFIX="${4:-com.alternatenetworktechnologies}"
 
 echo "Customizing macOS app with:"
 echo "  App Name: $APP_NAME"
 echo "  Manufacturer: $MANUFACTURER"
-echo "  Bundle ID: $BUNDLE_ID_PREFIX.$BUNDLE_ID_SUFFIX"
+echo "  Bundle ID: com.carriez.rustdesk (keeping original)"
 
 # Update Info.plist
 echo "Updating Info.plist..."
 # Note: macOS sed requires -i '' for in-place editing
+# Only update the app name, keep the bundle ID and URL scheme as is
 sed -i '' "s/<string>RustDesk<\/string>/<string>$APP_NAME<\/string>/g" flutter/macos/Runner/Info.plist
-sed -i '' "s/com\.carriez\.rustdesk/$BUNDLE_ID_PREFIX.$BUNDLE_ID_SUFFIX/g" flutter/macos/Runner/Info.plist
-sed -i '' "s/<string>rustdesk<\/string>/<string>$BUNDLE_ID_SUFFIX<\/string>/g" flutter/macos/Runner/Info.plist
 
 # Update project.pbxproj
 echo "Updating project.pbxproj..."
@@ -26,7 +23,7 @@ sed -i '' "s/RustDesk\.app/$APP_NAME.app/g" flutter/macos/Runner.xcodeproj/proje
 sed -i '' "s/PRODUCT_NAME = \"RustDesk\"/PRODUCT_NAME = \"$APP_NAME\"/g" flutter/macos/Runner.xcodeproj/project.pbxproj
 sed -i '' "s/productName = RustDesk/productName = $APP_NAME/g" flutter/macos/Runner.xcodeproj/project.pbxproj
 sed -i '' "s/\"RustDesk\"/\"$APP_NAME\"/g" flutter/macos/Runner.xcodeproj/project.pbxproj
-sed -i '' "s/PRODUCT_BUNDLE_IDENTIFIER = com\.carriez\.rustdesk/PRODUCT_BUNDLE_IDENTIFIER = $BUNDLE_ID_PREFIX.$BUNDLE_ID_SUFFIX/g" flutter/macos/Runner.xcodeproj/project.pbxproj
+# Keep the original bundle identifier - don't change it
 sed -i '' "s/PRODUCT_COPYRIGHT = \"Copyright © [^\"]*\"/PRODUCT_COPYRIGHT = \"Copyright © $(date +%Y) $MANUFACTURER\"/g" flutter/macos/Runner.xcodeproj/project.pbxproj
 
 # Update Runner.xcworkspace if needed
